47517ea00cf31441db6ef5fc99cb7c55c1c75ce4
Igorlizaso <76405774+Igorlizaso@users.noreply.github.com>
2021-04-18 17:07:40 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+const http = require("http");
+const express = require("express");
+const app = express();
+
+const server = app.listen(7000, () => {
+  console.log(`Express running → PORT ${server.address().port}`);
+});
+
+app.get('/', (req, res) => {
+  res.send('Bot de Musica sin interrupciones activadisimo');
+});
+
+require("dotenv").config();//Loading .env
+const fs = require("fs");
+const { Collection, Client } = require("discord.js");
+
+const client = new Client();//Making a discord bot client
+client.commands = new Collection();//Making client.commands as a Discord.js Collection
+client.queue = new Map()
+
+client.config = {
+  prefix: "tm!"
+}
+
+//Loading Events
+fs.readdir(__dirname + "/events/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach((file) => {
+    const event = require(__dirname + `/events/${file}`);
+    let eventName = file.split(".")[0];
+    client.on(eventName, event.bind(null, client));
+    console.log("Loading Event: "+eventName)
+  });
+});
+
+//Loading Commands
+fs.readdir("./commands/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach((file) => {
+    if (!file.endsWith(".js")) return;
+    let props = require(`./commands/${file}`);
+    let commandName = file.split(".")[0];
+    client.commands.set(commandName, props);
+    console.log("Loading Command: "+commandName)
+  });
+});
+
+//Logging in to discord
+const token = process.env['token']
+client.login(token); // Log the bot in
